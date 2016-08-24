3131ecf302ef4b67e139768a08d1157cdb1e0762
Steeve Postemus <spostemus@gmail.com>
2016-08-24 14:30:21 -0700

    added support for Devise testing

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
+  let (:wiki) { Wiki.create!(title: "New Wiki Title", body: "This is a new wiki that I created", private: false) }
 
+  describe "attributes" do
+    it "has title and body attributes" do
+      expect(wiki).to have_attributes(title: "New Wiki Title", body: "This is a new wiki that I created", private: false)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,12 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+
+require 'devise'
+require 'support/controller_macros'
+require 'factory_girl_rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -27,6 +32,11 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, type: :controller
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
