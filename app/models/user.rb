8242f7c0437de093db5ace9a9ccbc25a3697ec18
Steeve Postemus <spostemus@gmail.com>
2016-08-29 14:03:38 -0700

    added collab views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,25 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   has_many :wikis
+  has_many :collaborators
+  has_many :wikis, through: :collaborators
 
   enum role: [:standard, :premium, :admin ]
   after_initialize :set_default_role
 
   def set_default_role
     self.role ||= :standard
+  end
+
+  after_update :publicize_wikis
+
+  def publicize_wikis
+    if self.role == 'standard'
+      user_wikis = self.wikis.where(private: true)
+      user_wikis.each do |wiki|
+      wiki.update_attributes(private: false)
+      end
+    end
   end
 
   # Include default devise modules. Others available are:
